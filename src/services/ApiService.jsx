37eafa9316b88f2b37dceccc5b3592d0cbcf52d3
Bagabandi <218715730+Bagaa152lk@users.noreply.github.com>
2025-08-06 03:00:50 +0800

    api updated

--- src/services/ApiService.jsx
+++ src/services/ApiService.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://sudalgaa.jobs-pwd.gov.mn/api",
+  // baseURL: "http://sudalgaa.jobs-pwd.gov.mn/api",
+  baseURL: "/api",
 });
 
 const ApiService = async (method, url, data, headers = {}, responseType) => {
