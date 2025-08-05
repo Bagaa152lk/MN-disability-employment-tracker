2a3bbd80151e9a4e92eef0a6b237cae7318b909a
Bagaa <bagaa@sync.mn>
2025-08-05 20:57:10 +0800

    updated

--- src/services/ApiService.jsx
+++ src/services/ApiService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://sudalgaa.jobs-pwd.gov.mn/api",
+  baseURL: "/api",
 });
 
 const ApiService = async (method, url, data, headers = {}, responseType) => {
